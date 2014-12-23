475f820430f6d8c6749bde0eb1da80eb20d15f79
sultee <sultan@MacBook-Air-Sultan-2.local>
2014-12-24 01:26:42 +0200

    Sites list
    Moved sites list to sites_list

--- blocked.py
+++ blocked.py
@@ -8,12 +8,16 @@ bazar = urllib2.urlopen(adr).read()
 baza = bazar.splitlines()
 
 check_list = []
+sites_list = ['meduza', 'vk.com', 'twitter', 'facebook', 'google', '.io', 'yandex', 'rambler']
+
 
 for i in baza:
-	if 'meduza' in i or 'facebook' in i or 'vk.com' in i or 'twitter' in i or '.io' in i:
-		infolink = 'http://api.antizapret.info/get.php?item=' + str(i) + '&type=json'
-		info = json.load(urllib2.urlopen(infolink))
-		check_list.append(i+ '  Included  ' + info['register'][0]['includeTime'] + '  by  ' + info['register'][0]['org'] + '  url:  ' + info['register'][0]['url'])
+        for site in sites_list:
+                if site in i:
+#	if 'meduza' in i or 'facebook' in i or 'vk.com' in i or 'twitter' in i or '.io' in i:
+                        infolink = 'http://api.antizapret.info/get.php?item=' + str(i) + '&type=json'
+                        info = json.load(urllib2.urlopen(infolink))
+                        check_list.append(i+ '  Included  ' + info['register'][0]['includeTime'] + '  by  ' + info['register'][0]['org'] + '  url:  ' + info['register'][0]['url'])
 
 
 
